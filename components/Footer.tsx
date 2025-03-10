26fce5586ff211fcae48a47be01e27066ebf881d
MohanCodes <mohanatkuri@gmail.com>
2025-03-09 19:57:41 -0500

    new disc link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     { name: 'Mail', href: 'mailto:metexcellence@gmail.com', icon: FaEnvelope },
     { name: 'Instagram', href: 'https://www.instagram.com/metexcellence/', icon: PiInstagramLogoFill },
     { name: 'YouTube', href: 'https://youtube.com/@METExcellence', icon: FaYoutube },
-    { name: 'Discord', href: 'https://discord.gg/a74jRnwEAT', icon: FaDiscord },
+    { name: 'Discord', href: 'https://discord.gg/rtWSXrVUzr', icon: FaDiscord },
     { name: 'Twitter', href: 'https://twitter.com/metexcellence', icon: PiXLogoBold },
   ];
   
